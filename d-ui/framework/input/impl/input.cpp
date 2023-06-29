cc874c26be522a9c627c9e219179feae93bd83dd
Dutu1337 <46423190+dumitru1216@users.noreply.github.com>
2023-06-29 16:19:07 -0700

    g_clicking

--- d-ui/framework/input/impl/input.cpp
+++ d-ui/framework/input/impl/input.cpp
@@ -41,3 +41,34 @@ bool handling::impl::g_hovering( const sdk::rect_t& area, bool from_start,
 	return g_mouse_pos.x >= area.x && g_mouse_pos.y >= area.y && g_mouse_pos.x <= area.x + area.w && 
 		g_mouse_pos.y <= area.y + area.h && !g_clip_arrival( g_mouse_pos, overlay );
 }
+
+bool handling::impl::g_clicking( const sdk::rect_t& area, bool from_start,
+				 bool override, bool overlay ) {
+	if ( handling::input_sdk::g_finished_input_frame ) {
+		return false;
+	}
+
+	/* handle mouse position */
+	sdk::pos_t g_mouse_pos{};
+	warp::bindings::g_mouse_pos( g_mouse_pos );
+
+	/* handle it */
+	if ( !handling::input_sdk::g_click_switch && GetAsyncKeyState( VK_LBUTTON ) && !g_clip_arrival( g_mouse_pos, overlay ) ) { /* press key */
+		handling::input_sdk::g_click_start = g_mouse_pos;
+
+		if ( from_start ) {
+			if ( g_hovering( area, true, override, overlay ) ) {
+				handling::input_sdk::g_click_switch = true;
+				return true;
+			}
+		} else if ( g_hovering( area, false, override, overlay ) ) {
+			handling::input_sdk::g_click_switch = true;
+			handling::input_sdk::g_finished_input_frame = true;
+			return true;
+		}
+	} else if ( handling::input_sdk::g_click_switch && !GetAsyncKeyState( VK_LBUTTON ) ) { /* release key */
+		handling::input_sdk::g_click_start = sdk::pos_t( 0, 0 );
+	}
+
+	return false;
+}
