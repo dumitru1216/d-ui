bdcab51d6a49b9f2939425c99509cee927ac9a52
Dutu1337 <46423190+dumitru1216@users.noreply.github.com>
2023-07-07 12:18:48 -0700

    group #1

---
+++ d-ui/framework/objects/group/cpp/group.cpp
@@ -0,0 +1,10 @@
+#include "../group.hpp"
+#include "../../window/window.hpp"
+#include "../../../theme/theme.hpp"
+
+void ui::group::think( ) {
+	auto& g_parent_window = g_find_parent< window >( g_object_window );
+
+	/* we are going to handle the animation later */
+
+}
---
+++ d-ui/framework/objects/group/group.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include "../../../includes.hpp"
+#include "../obj/obj.hpp"
+
+namespace ui {
+	class group : public obj {
+	private:
+		double g_scroll_offset = 0.0;
+		int g_max_height = 0;
+	public:
+		std::vector< std::shared_ptr< obj > > g_objects;
+		std::string g_title;
+
+		/* constructor */
+		group( const std::string& g_title ) {
+			this->g_area = g_area;
+			this->g_title = g_title;
+			g_type = g_object_group;
+		}
+		~group( ) { }
+
+		void g_handle_element( const std::shared_ptr< obj >& g_new_obj ) {
+			g_new_obj->g_parent = this;
+			g_objects.push_back( g_new_obj );
+		}
+
+		void think( );
+
+		/* override draw */
+		void g_draw( ) override;
+	};
+}
